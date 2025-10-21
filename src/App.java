440d9d8765ffdf9a70d9481d266496b96c394fd3
Bryan <bryanbarrosvilla@gmail.com>
2025-10-21 09:28:21 -0500

    Practica en clase 2

--- src/App.java
+++ src/App.java
@@ -1,19 +1,31 @@
 public class App {
-    public static void main(String[] args) {
-        int[] numeros = {10, 0, -5, 5, 15, 2};
-
+    public static void main(String[] args) throws Exception {
+        int[] numeros ={10, 0, -5, 5, 15, 2};
+        System.out.println("Estudiante: Bryan Barros");
         SortBubble claseSortBubble = new SortBubble();
-
-        System.out.println("Array");
-        System.out.println("10, 0, -5, 5, 15, 2");
-
-        System.out.println("\nAscendente:");
-        claseSortBubble.sortBubbleAscedente(numeros);
-        claseSortBubble.printArray(numeros);
-
-        System.out.println("\nDescendente:");
-        claseSortBubble.sortBubbleDescendente(numeros);
-        claseSortBubble.printArray(numeros);
+        SortSelection claseSelection = new SortSelection();
         
+       // System.out.println("Arreglo Original: ");
+        // claseSortBubble.printArray(numeros); 
+       // System.out.println(" Ordenado Ascendente: ");
+        //claseSortBubble.sortBubbleAscedente(numeros); 
+        //claseSortBubble.printArray(numeros);
+        //System.out.println("Ordenado Descencente: ");
+        //claseSortBubble.sortBubbleDescendente(numeros);
+        //claseSortBubble.printArray(numeros);
+
+        System.out.println("-- Metodo Selection -- ");
+
+        System.out.println("Arreglo Original: ");
+        claseSortBubble.printArray(numeros); 
+        claseSelection.sortAscendente(numeros);
+
+        System.out.println("Arreglo Ordenado Ascendente:");
+        claseSelection.printArray(numeros);
+        claseSelection.sortDescendente(numeros);
+        
+        System.out.println("Arreglo Ordenado Descendente: ");
+        claseSelection.printArray(numeros);
+
     }
-}
+}   
